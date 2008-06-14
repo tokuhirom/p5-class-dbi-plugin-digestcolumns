1a51286f14cde15a2845f7e5e7840ecf3ea72379
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-14 05:37:14 +0000

    lang/perl/mobirc: Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 20820)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Class::DBI::Plugin::DigestColumns
     
    +0.03
    +
    +    - skip tests if DBD::SQLite is not available
    +    - some tests move to xt/
    +
     0.02  Wed Mar 08 15:07:29 2006
         - add prereqs.
     
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Class-DBI-Plugin-DigestColumns/trunk@13877 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Class/DBI/Plugin/DigestColumns.pm
+++ lib/Class/DBI/Plugin/DigestColumns.pm
@@ -1,7 +1,7 @@
 package Class::DBI::Plugin::DigestColumns;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use base qw/Class::DBI::Plugin/;
 use Scalar::Util qw/blessed/;
 use Digest::SHA1 ();
